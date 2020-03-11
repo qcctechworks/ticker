d88fcc498818f7cb624ec81e1d6c8ee67ea0e606
qcctechworks <qcctechworks@gmail.com>
2020-03-10 20:25:34 -0400

    Added the componentDidMount and shouldComponentUpdate lifecycle methods and the reset method yay :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class Ticker extends Component {
+  constructor () {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+
+  //LifeCycle methods
+  componentDidMount () {
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1,
+      })
+    }, 1000);
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    return (nextState.count % 3 === 0)
+  }
+
+
+  //this is a custom method
+  reset = () => {
+    this.setState({
+      count: 0,
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <p>The ticker is: {this.state.count}</p>
+        <button onClick={this.reset} type="button">Reset the Ticker</button>
+      </div>
+    );
+  };
+};
+
+export default Ticker;
